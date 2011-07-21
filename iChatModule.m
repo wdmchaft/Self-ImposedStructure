e20557a49cad44061aa35a80c73a2f5523b72739
Charles Eubanks <charles.eubanks@gmail.com>
2011-07-20 22:49:41 -0700

    Do not assume ichat is running -- b/c if it isn't running calling applescript will start it

--- iChatModule.m
+++ iChatModule.m
@@ -19,27 +19,29 @@
 
 - (void) changeState: (WPAStateType) newState
 {
-	
-	iChatMyStatus newStatus;
-	switch (newState) {
-		case WPASTATE_THINKING:
-			newStatus = iChatMyStatusInvisible;
-			break;
-		case WPASTATE_AWAY:
-			newStatus = iChatMyStatusAway;
-			break;
-		case WPASTATE_FREE:
-			newStatus = iChatMyStatusAvailable;
-			break;
-		default:
-			break;
-	}
-	iChatApplication *iChatApp = [SBApplication applicationWithBundleIdentifier:@"com.apple.iChat"];
-	NSArray *services = [iChatApp services];
-	for (iChatService *service in services)
-	{
-		service.status = newStatus;
-	}
+    NSArray *chatApps = [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.apple.iChat"];
+    if (chatApps && [chatApps count] > 0) {
+        iChatMyStatus newStatus;
+        switch (newState) {
+            case WPASTATE_THINKING:
+                newStatus = iChatMyStatusInvisible;
+                break;
+            case WPASTATE_AWAY:
+                newStatus = iChatMyStatusAway;
+                break;
+            case WPASTATE_FREE:
+                newStatus = iChatMyStatusAvailable;
+                break;
+            default:
+                break;
+        }
+        iChatApplication *iChatApp = [SBApplication applicationWithBundleIdentifier:@"com.apple.iChat"];
+        NSArray *services = [iChatApp services];
+        for (iChatService *service in services)
+        {
+            service.status = newStatus;
+        }
+    }
 }
 
 -(void) startValidation: (NSObject*) callback  
